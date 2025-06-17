64ff7be3d94b4a4dd1d42197cc569cc381c26a84
Shivam Kachole <eshivamkachole9312@gmail.com>
2025-06-17 18:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def hello():
     return '''
-        <!DOCTYPE html>
-        <html>
-        <body>
-            <h1>My first Flask page version via Webhooks</h1>
-            <p>hello World from test FROM S2I 2nd time VERSION 3.2 LEARNING OPENSHIFT 4.14 new code</p>
-        </body>
-        </html>
+    <h1>My first Flask page version via Webhooks</h1>
+    hello World from test FROM S2I 2nd time VERSION 3.2 LEARNING OPENSHIFT 4.14 new code
     '''
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
